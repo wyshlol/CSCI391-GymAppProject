f3a1147c56a9c839cec10e03c170d243c5074ef0
Cameron Christofferson <cc147209@umconnect.umt.edu>
2022-10-10 14:14:17 -0600

    Progress Commit

--- app/src/main/java/com/example/csci391gymappproject/model/Workout.kt
+++ app/src/main/java/com/example/csci391gymappproject/model/Workout.kt
@@ -1,8 +1,7 @@
 package com.example.csci391gymappproject.model
 
-import androidx.annotation.DrawableRes
 import androidx.annotation.StringRes
 
-data class Workout(val stringResourceId: Int) {
+data class Workout(@StringRes val stringResourceId: Int) {
 
 }
